37733c0fb91b1c12ae8ba951e03b22bb8fd90750
daehwan0307 <eoghks960307@naver.com>
2022-03-03 09:55:21 +0900

    tailwind 사용

--- pages/login.js
+++ pages/login.js
@@ -4,10 +4,11 @@ const LoginPage=()=>{
     return (
         
         <div>
-             <p>이메일을 입력해 주세요</p>
+             <p>이메일 입력</p>
              <input type="text"  placeholder="이메일을 입력해주세요"/><br></br>
-             <p>비밀번호를 입력해 주세요</p>
+             <p>비밀번호 입력</p>
              <input type="password" placeholder="비밀번호를 입력해주세요"/>
+           
            <hr></hr>
             <a>비밀번호 찾기</a>
 
@@ -16,4 +17,4 @@ const LoginPage=()=>{
     );
 }
 
-export default LoginPage;
+export default LoginPage;npm
