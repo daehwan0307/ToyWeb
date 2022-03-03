c209427402d24c5faa74a5cbf320cc952b0e5c65
daehwan0307 <eoghks960307@naver.com>
2022-03-03 18:49:46 +0900

    ant design 제거

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,17 @@
 import KakaoLoginButton from "../componenents/KakaoLoginButton";
 import Link from "next/link";
-import { Row, Col } from 'antd';
-import 'antd/dist/antd.css';
-
-
-
 
 const API_KEY = "10923b261ba94d897ac6b81148314a3f";
 
-
 const  Home=()=>{
-
     return (
-       
         <>
-        <Row>
-            <Col span={24}><center><img src="/hihi.jpg"  /></center></Col>
-            
-        </Row>
-        <Row>
-            <Col span={24}><center><KakaoLoginButton/></center></Col>
-        </Row>
-        <Row>
-            <Col span={12}><Link href="/signin" passHref><center><a><img src=""></img>이메일로 회원가입</a></center></Link></Col>
-            <Col span={12}> <Link href="/login" passHref><center><a>이메일로 로그인</a></center></Link></Col>
-        </Row>
-        <Row>
-            <Col span={24}><Link href="/main" passHref><center><a>main page(제작중)</a></center></Link></Col>
-        </Row>
+            <center><img src="/hihi.jpg"  /></center>
+            <center><KakaoLoginButton/></center>
+            <Link href="/signin" passHref><center><a><img src=""></img>이메일로 회원가입</a></center></Link>
+            <Link href="/login" passHref><center><a>이메일로 로그인</a></center></Link>
+            <Link href="/main" passHref><center><a>main page(제작중)</a></center></Link>
         </>
-
-    
     );
 }
 export default Home;
