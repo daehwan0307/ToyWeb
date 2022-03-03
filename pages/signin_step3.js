a55fb5fc2371fe27e926ba9517f35569572d12f2
daehwan0307 <eoghks960307@naver.com>
2022-03-03 16:35:24 +0900

    페이징처리

--- pages/signin_step3.js
+++ pages/signin_step3.js
@@ -9,7 +9,7 @@ const signin_step3 = ()=>{
         <h4>홍길동님 회원가입이 완료되었습니다.</h4>
         <br/>
         <h4>지금부터 윈윈을 통해 재능교환을 진행하실 수 있습니다.</h4>
-        <Link href="/signin_step3" passHref><center><button><a>로그인</a></button></center></Link>
+        <Link href="/main" passHref><center><button><a>로그인</a></button></center></Link>
         </>
     );
 }
